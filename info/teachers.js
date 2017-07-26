4f6717f5432bf2bc7aefec6a29ace0b83c4bdc80
saltyJeff <saltyJeff@users.noreply.github.com>
2017-07-25 23:24:40 -0700

    added eugene

--- info/teachers.js
+++ info/teachers.js
@@ -1,5 +1,10 @@
 module.exports = [
 	{
+		img: "./img/eugene.jpg",
+		name: "Eugene Hwang",
+		bio: "I am a high school Junior who is currently learning the touches of making a website."
+	},
+	{
 		img: "./img/jefferson.jpg",
 		name: "Jefferson Lee",
 		bio: "I'm a high school senior and a self taught programmer who dabbles in Java and C#/Unity3d. I also do front/backend development with many libraries + Node.js and Mongo. "+
--- public/bundle.js
+++ public/bundle.js
@@ -3616,6 +3616,11 @@ route.parser();
 
 module.exports = [
 	{
+		img: "./img/eugene.jpg",
+		name: "Eugene Hwang",
+		bio: "I am a high school Junior who is currently learning the touches of making a website."
+	},
+	{
 		img: "./img/jefferson.jpg",
 		name: "Jefferson Lee",
 		bio: "I'm a high school senior and a self taught programmer who dabbles in Java and C#/Unity3d. I also do front/backend development with many libraries + Node.js and Mongo. "+
